3984c7d532fe67784b9d07156a676dbf6b0f00f8
heewookji <jhu2269@gmail.com>
2023-06-27 10:58:58 +0900

    fix: dragDistance 더 짧게 수정

--- packages/webview_flutter/webview_flutter_android/lib/src/initial_gesture_recognizer.dart
+++ packages/webview_flutter/webview_flutter_android/lib/src/initial_gesture_recognizer.dart
@@ -20,7 +20,8 @@ class InitialDragGestureRecognizer extends OneSequenceGestureRecognizer {
   AndroidViewController androidViewController;
 
   /// 드래그 판단 기준
-  final double dragDistance = 18.0;
+  final double dragDistance = 10.0;
+
   bool _isDragging = false;
   PointerDownEvent? _initialEvent;
 
